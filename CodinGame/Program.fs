72eab21fd6df1737f58dd7bac10862ee753b91ef
Usein Mambediiev <useinm.gmail.com>
2016-09-28 22:43:45 -0700

    Mars lander - episode 1

--- CodinGame/Program.fs
+++ CodinGame/Program.fs
@@ -8,39 +8,47 @@ open System
 
 [<EntryPoint>]
 let main argv =
+
+    let powerLevels =
+        [
+            (0, fun x -> x > 0);
+            (1, fun x -> x > -10);
+            (2, fun x -> x > -20);
+            (3, fun x -> x > -40);
+            (4, fun x -> x <= -40)
+        ]
     
-    let take n lst =
-        let rec takeInner n source res =
-            if n <= 0 then res else
-                match source with
-                | [] -> res
-                | x::xs -> takeInner (n-1) xs (x::res)
-        takeInner n lst []
+    let surfaceN = int(Console.In.ReadLine()) (* the number of points used to draw the surface of Mars. *)
+    for i in 0 .. surfaceN - 1 do
+        (* landX: X coordinate of a surface point. (0 to 6999) *)
+        (* landY: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars. *)
+        let token = (Console.In.ReadLine()).Split [|' '|]
+        let landX = int(token.[0])
+        let landY = int(token.[1])
+        ()
 
-    let groupBy f lst =
-        lst
-        |> List.fold (fun group x ->
-            match group |> Map.tryFind (f x) with
-            | Some(s) -> group |> Map.remove (f x) |> Map.add (f x) (x::s)
-            | None -> group |> Map.add (f x) [x]
-            ) Map.empty
-        |> Map.toList
 
-    let readTemps n =
-        let temps = Console.In.ReadLine()
-        temps.Split([|' '|]) |> List.ofArray |> take n |> List.map (fun s -> int(s))
+    (* game loop *)
+    while true do
+        (* hSpeed: the horizontal speed (in m/s), can be negative. *)
+        (* vSpeed: the vertical speed (in m/s), can be negative. *)
+        (* fuel: the quantity of remaining fuel in liters. *)
+        (* rotate: the rotation angle in degrees (-90 to 90). *)
+        (* power: the thrust power (0 to 4). *)
+        let token1 = (Console.In.ReadLine()).Split [|' '|]
+        let X = int(token1.[0])
+        let Y = int(token1.[1])
+        let hSpeed = int(token1.[2])
+        let vSpeed = int(token1.[3])
+        let fuel = int(token1.[4])
+        let rotate = int(token1.[5])
+        let power = int(token1.[6])
+    
+        (* Write an action using printfn *)
+        (* To debug: Console.Error.WriteLine("Debug message") *)
+        let newPower =
+            powerLevels |> List.find (fun (_, f) -> f vSpeed) |> fst
 
-    let groupTemps temps =
-        temps |> groupBy (fun x -> abs(x))
-
-    let n = int(Console.In.ReadLine()) (* the number of temperatures to analyse *)
-    if n > 0 then
-        let temps = readTemps n (* the n temperatures expressed as integers ranging from -273 to 5526 *)
-
-        let closestGroup = temps |> groupTemps |> List.minBy (fun (x, _) -> x)
-
-        printfn "%d"
-            (closestGroup |> fun (x, xs) -> xs |> List.max)
-    else
-        printfn "0"
+        (* 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4). *)
+        printfn "0 %d" newPower
     0
