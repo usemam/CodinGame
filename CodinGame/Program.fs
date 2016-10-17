9bd73c5a8a9e8cc232791e665ee803f10e5d6f2b
Usein Mambediiev <useinm.gmail.com>
2016-10-16 22:49:32 -0700

    Shadows of the Knight - Episode 1

--- CodinGame/Program.fs
+++ CodinGame/Program.fs
@@ -5,86 +5,76 @@ open System
 [<EntryPoint>]
 let main argv =
     
-    let contains i l =
-        l |> List.fold (fun r x -> r || x = i) false
-
-    let rec lastN n l =
-        match List.length l <= 2 with
-        | true -> l
-        | false -> lastN n l.Tail
-
-    (* N: the total number of nodes in the level, including the gateways *)
-    (* L: the number of links *)
-    (* E: the number of exit gateways *)
+    (* W: width of the building. *)
+    (* H: height of the building. *)
     let token = (Console.In.ReadLine()).Split [|' '|]
-    let N = int(token.[0])
-    let L = int(token.[1])
-    let E = int(token.[2])
+    let W = int(token.[0])
+    let H = int(token.[1])
+    let N = int(Console.In.ReadLine()) (* maximum number of turns before game over. *)
+    let token1 = (Console.In.ReadLine()).Split [|' '|]
+    let X0 = int(token1.[0])
+    let Y0 = int(token1.[1])
 
-    let nodes = [0..N-1]
-    let edges =
-        [0..L-1] |> List.map (fun i ->
-            (* N1: N1 and N2 defines a link between these nodes *)
-            let token1 = (Console.In.ReadLine()).Split [|' '|]
-            let N1 = int(token1.[0])
-            let N2 = int(token1.[1])
-            (N1, N2))
+    let up bat a b c d =
+        let x = fst bat
+        let y = (snd bat + snd a)/2
+        ((x, y), (x, snd a), (x, snd a), bat, bat)
+    let upRight bat a b c d =
+        let x = (fst bat + fst b)/2
+        let y = (snd bat + snd a)/2
+        ((x, y), (fst bat, snd a), b, bat, (fst b, snd bat))
+    let right bat a b c d =
+        let x = (fst bat + fst b)/2
+        let y = snd bat
+        ((x, y), bat, (fst b, y), bat, (fst b, y))
+    let downRight bat a b c d =
+        let x = (fst bat + fst b)/2
+        let y = (snd bat + snd c)/2
+        ((x, y), bat, (fst b, snd bat), (fst bat, snd c), d)
+    let down bat a b c d =
+        let x = fst bat
+        let y = (snd bat + snd c)/2
+        ((x, y),  bat, bat, (x, snd c), (x, snd c))
+    let downLeft bat a b c d =
+        let x = (fst a + fst bat)/2
+        let y = (snd bat + snd c)/2
+        ((x, y), (fst a, snd bat), bat, c, (fst bat, snd d))
+    let left bat a b c d =
+        let x = (fst a + fst bat)/2
+        let y = snd bat
+        ((x, y), (fst a, y), bat, (fst a, y), bat)
+    let upLeft bat a b c d =
+        let x = (fst a + fst bat)/2
+        let y = (snd b + snd bat)/2
+        ((x, y), a, (fst bat, snd a), (fst a, snd bat), bat)
 
-    let getAdjacent n =
-        edges |> List.fold (fun l (x, y) ->
-            match x = n with
-            | true -> y::l
-            | false ->
-                match y = n with
-                | true -> x::l
-                | false -> l) []
-    let mutable graph =
-        nodes |> List.fold (fun g i ->
-            Map.add i (getAdjacent i) g) Map.empty
+    let rec bombBinarySearch bat a b c d =
+        let getJumpSlotAndNewRect dir =
+            let dirMap =
+                [
+                    ("U", up);
+                    ("UR", upRight);
+                    ("R", right);
+                    ("DR", downRight);
+                    ("D", down);
+                    ("DL", downLeft);
+                    ("L", left);
+                    ("UL", upLeft)
+                ]
+            let mutator =
+                dirMap
+                |> List.find (fun (x, _) -> x = dir)
+                |> snd
+            mutator bat a b c d
 
-    let gateways =
-        [0..E-1] |> List.fold (fun l i ->
-            let EI = int(Console.In.ReadLine()) (* the index of a gateway node *)
-            EI::l) []
-    let isGateway i =
-        contains i gateways
+        let bombDir = Console.In.ReadLine().ToUpper()
 
-    let removeEdge g (x, y) =
-        let ex = Map.find x g
-        let ex' = List.filter (fun e -> e <> y) ex
-        let g' = g |> ((Map.remove x) >> (Map.add x ex'))
-        let ey = Map.find y g'
-        let ey' = List.filter (fun e -> e <> x) ey
-        g' |> ((Map.remove y) >> (Map.add y ey'))
+        let ((x, y), a', b', c', d') = getJumpSlotAndNewRect bombDir
 
-    let getShortestPath g n =
-        let rec bfs queue =
-            match List.isEmpty queue with
-            | true -> failwith "Failed to find a path"
-            | false ->
-                let path = List.head queue
-                let current = Seq.last path
-                match isGateway current with
-                | true -> path
-                | false ->
-                    let q' = List.tail queue
-                    let notVisited =
-                        Map.find current g
-                        |> List.filter (fun i -> not (contains i path))
-                    notVisited
-                    |> List.fold (fun q i -> q@[path@[i]]) q'
-                    |> bfs
-        bfs [[n]]
-
-    (* game loop *)
-    while true do
-        let SI = int(Console.In.ReadLine()) (* The index of the node on which the Skynet agent is positioned this turn *)
-
-        let shortest = getShortestPath graph SI
-
-        let [x;y;] = lastN 2 shortest
-        graph <- removeEdge graph (x, y)
-        (* Example: 0 1 are the indices of the nodes you wish to sever the link between *)
         printfn "%d %d" x y
-        ()
+
+        bombBinarySearch (x, y) a' b' c' d'
+    
+    bombBinarySearch (X0, Y0) (0,0) (W-1,0) (0,H-1) (W-1,H-1)
+    
     0
