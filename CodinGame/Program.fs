1782735d041a4a2eb838bf2050f479547c6d3d77
Usein Mambediiev <useinm.gmail.com>
2016-10-23 11:25:02 -0700

    Teads challenge - 80% score doesn't scale on large graphs :(

--- CodinGame/Program.fs
+++ CodinGame/Program.fs
@@ -5,76 +5,68 @@ open System
 [<EntryPoint>]
 let main argv =
     
-    (* W: width of the building. *)
-    (* H: height of the building. *)
-    let token = (Console.In.ReadLine()).Split [|' '|]
-    let W = int(token.[0])
-    let H = int(token.[1])
-    let N = int(Console.In.ReadLine()) (* maximum number of turns before game over. *)
-    let token1 = (Console.In.ReadLine()).Split [|' '|]
-    let X0 = int(token1.[0])
-    let Y0 = int(token1.[1])
+    let n = int(Console.In.ReadLine()) (* the number of adjacency relations *)
+    let edges =
+        [0..n-1]
+        |> List.map (fun i -> Console.In.ReadLine())
+        |> List.map (fun s -> s.Split [|' '|])
+        |> List.map (fun l -> (l.[0] |> int, l.[1] |> int))
 
-    let up bat a b c d =
-        let x = fst bat
-        let y = (snd bat + snd a)/2
-        ((x, y), (x, snd a), (x, snd a), bat, bat)
-    let upRight bat a b c d =
-        let x = (fst bat + fst b)/2
-        let y = (snd bat + snd a)/2
-        ((x, y), (fst bat, snd a), b, bat, (fst b, snd bat))
-    let right bat a b c d =
-        let x = (fst bat + fst b)/2
-        let y = snd bat
-        ((x, y), bat, (fst b, y), bat, (fst b, y))
-    let downRight bat a b c d =
-        let x = (fst bat + fst b)/2
-        let y = (snd bat + snd c)/2
-        ((x, y), bat, (fst b, snd bat), (fst bat, snd c), d)
-    let down bat a b c d =
-        let x = fst bat
-        let y = (snd bat + snd c)/2
-        ((x, y),  bat, bat, (x, snd c), (x, snd c))
-    let downLeft bat a b c d =
-        let x = (fst a + fst bat)/2
-        let y = (snd bat + snd c)/2
-        ((x, y), (fst a, snd bat), bat, c, (fst bat, snd d))
-    let left bat a b c d =
-        let x = (fst a + fst bat)/2
-        let y = snd bat
-        ((x, y), (fst a, y), bat, (fst a, y), bat)
-    let upLeft bat a b c d =
-        let x = (fst a + fst bat)/2
-        let y = (snd b + snd bat)/2
-        ((x, y), a, (fst bat, snd a), (fst a, snd bat), bat)
+    let nodes =
+        edges
+        |> List.fold (fun l (x, y) -> x::y::l) []
+        |> Seq.distinct
+        |> List.ofSeq
 
-    let rec bombBinarySearch bat a b c d =
-        let getJumpSlotAndNewRect dir =
-            let dirMap =
-                [
-                    ("U", up);
-                    ("UR", upRight);
-                    ("R", right);
-                    ("DR", downRight);
-                    ("D", down);
-                    ("DL", downLeft);
-                    ("L", left);
-                    ("UL", upLeft)
-                ]
-            let mutator =
-                dirMap
-                |> List.find (fun (x, _) -> x = dir)
-                |> snd
-            mutator bat a b c d
+    let getAdjacent n =
+        edges
+        |> List.fold (fun l (x, y) ->
+            match x = n with
+            | true -> y::l
+            | false ->
+                match y = n with
+                | true -> x::l
+                | false -> l) []
+    let graph =
+        nodes
+        |> List.fold (fun g i ->
+            Map.add i (getAdjacent i) g) Map.empty
 
-        let bombDir = Console.In.ReadLine().ToUpper()
+    let getMostDistantNode g n =
+        let contains i l =
+            List.fold (fun r x -> x = i || r) false l
+        let rec dfs stack paths =
+            match List.isEmpty stack with
+            | true -> paths
+            | false ->
+                let path = List.head stack
+                let (current, distance) = List.head path
+                
+                let visited = path |> List.map (fun (x,_) -> x)
 
-        let ((x, y), a', b', c', d') = getJumpSlotAndNewRect bombDir
+                let notVisited =
+                    Map.find current g
+                    |> List.filter (fun x -> not (contains x visited))
+                match List.isEmpty notVisited with
+                | true ->
+                    dfs (List.tail stack) (path::paths)
+                | false ->
+                    let newStack =
+                        List.fold (fun s n -> ((n, distance+1)::path)::s) (List.tail stack) notVisited
+                    dfs newStack paths
 
-        printfn "%d %d" x y
+        let paths = dfs [[(n,0)]] []
+        let longestPath =
+            paths |> List.maxBy (fun l -> List.length l)
+        List.head longestPath
 
-        bombBinarySearch (x, y) a' b' c' d'
-    
-    bombBinarySearch (X0, Y0) (0,0) (W-1,0) (0,H-1) (W-1,H-1)
+    let startNode = List.head nodes
+    let secondNode = getMostDistantNode graph startNode
+    let thirdNode = getMostDistantNode graph (fst secondNode)
+    let graphD = snd thirdNode
+    (* The minimal amount of steps required to completely propagate the advertisement *)
+    printfn "%d" ((graphD + 1)/2)
+
+    Console.In.ReadLine()
     
     0
