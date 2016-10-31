7af7b013da4c30639cd6b22d3d537d3ad73bc1e2
Usein Mambediiev <useinm.gmail.com>
2016-10-30 20:09:00 -0700

    Don't panic - Episode 1

--- CodinGame/Program.fs
+++ CodinGame/Program.fs
@@ -2,66 +2,80 @@
 (* the standard input according to the problem statement. *)
 open System
 
+type Direction = Left | Right
+
 [<EntryPoint>]
 let main argv =
+
+    (* nbFloors: number of floors *)
+    (* width: width of the area *)
+    (* nbRounds: maximum number of rounds *)
+    (* exitFloor: floor on which the exit is found *)
+    (* exitPos: position of the exit on its floor *)
+    (* nbTotalClones: number of generated clones *)
+    (* nbAdditionalElevators: ignore (always zero) *)
+    (* nbElevators: number of elevators *)
+    let token = (Console.In.ReadLine()).Split [|' '|]
+    let nbFloors = int(token.[0])
+    let width = int(token.[1])
+    let nbRounds = int(token.[2])
+    let exitFloor = int(token.[3])
+    let exitPos = int(token.[4])
+    let nbTotalClones = int(token.[5])
+    let nbElevators = int(token.[7])
+
+    let isExitFloor n = exitFloor = n
+
+    let readElevatorPosition map =
+        let token1 = (Console.In.ReadLine()).Split [|' '|]
+        let elevatorFloor = int(token1.[0])
+        let elevatorPos = int(token1.[1])
+        Map.add elevatorFloor elevatorPos map
+    let elevators =
+        [0..nbElevators-1]
+        |> List.fold (fun m _ -> readElevatorPosition m) Map.empty
+
+    (* game loop *)
+    let readClonePositionAndPrintAction blocks =
+        let decideOnAction (floor, pos) (dir : Direction) =
+            let moveTo =
+                match isExitFloor floor with
+                | true -> exitPos
+                | false -> Map.find floor elevators
+            let moveToDirection =
+                match moveTo - pos with
+                | diff when diff > 0 -> Right
+                | diff when diff < 0 -> Left
+                | _ -> dir
+            match dir = moveToDirection with
+            | true -> "WAIT"
+            | false -> "BLOCK"
+
+        (* cloneFloor: floor of the leading clone *)
+        (* clonePos: position of the leading clone on its floor *)
+        (* direction: direction of the leading clone: LEFT or RIGHT *)
+        let token2 = (Console.In.ReadLine()).Split [|' '|]
+        let cloneFloor = int(token2.[0])
+        let clonePos = int(token2.[1])
+        let direction = token2.[2]
+
+        (* action: WAIT or BLOCK *)
+        let action =
+            match direction.ToUpper() with
+            | "NONE" -> "WAIT"
+            | "LEFT" -> decideOnAction (cloneFloor, clonePos) Left
+            | "RIGHT" -> decideOnAction (cloneFloor, clonePos) Right
+            | _ -> failwith "Unsupported direction"
     
-    let n = int(Console.In.ReadLine()) (* the number of adjacency relations *)
-    let edges =
-        [0..n-1]
-        |> List.map (fun _ -> Console.In.ReadLine())
-        |> List.map (fun s -> s.Split [|' '|])
-        |> List.map (fun l -> (l.[0] |> int, l.[1] |> int))
+        printfn "%s" action
+        let newBlocks =
+            match action with
+            | "BLOCK" -> (cloneFloor, clonePos)::blocks
+            | _ -> blocks
+        newBlocks
 
-    let (minNode, maxNode) =
-        edges
-        |> List.fold (fun l (x, y) -> x::y::l) []
-        |> Seq.distinct
-        |> Seq.sort
-        |> (fun s -> (Seq.head s, Seq.last s))
+    Seq.initInfinite (fun i -> i)
+    |> Seq.fold (fun s _ -> readClonePositionAndPrintAction s) []
+    |> ignore
 
-    let getAdjacent n =
-        edges
-        |> List.fold (fun l (x, y) ->
-            match x = n with
-            | true -> y::l
-            | false ->
-                match y = n with
-                | true -> x::l
-                | false -> l) []
-    let graph =
-        [| for i in minNode..maxNode do yield getAdjacent i |]
-
-    let getMostDistantNode (g: int list []) n =
-        let dMap = [| for i in minNode..maxNode do yield 0 |]
-        let rec dfs stack =
-            match List.isEmpty stack with
-            | true -> ()
-            | false ->
-                let current = List.head stack
-                let distance = dMap.[current - minNode]
-                let notVisited =
-                    g.[current - minNode]
-                    |> List.filter (fun x -> dMap.[x - minNode] = 0)
-                notVisited
-                |> List.iter (fun x -> dMap.[x - minNode] <- distance + 1)
-                let newStack =
-                    notVisited
-                    |> List.fold (fun s x -> x::s) (List.tail stack)
-                dfs newStack
-
-        dfs [n]
-        dMap
-        |> Array.mapi (fun i x -> (i, x))
-        |> Array.maxBy (fun (_, x) -> x)
-        |> fun (i, x) -> (i + minNode, x)
-        
-
-    let secondNode = getMostDistantNode graph minNode
-    let thirdNode = getMostDistantNode graph (fst secondNode)
-    let graphD = snd thirdNode
-    (* The minimal amount of steps required to completely propagate the advertisement *)
-    printfn "%d" ((graphD + 1)/2)
-
-    Console.In.ReadLine()
-    
     0
